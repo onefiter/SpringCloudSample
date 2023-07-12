c5711ba1574c45ca7e17d3344bdbca49ffe4ee69
onefiter <onefiter@gmail.com>
2023-07-12 23:01:07 +0800

    Eureka

---
+++ user-consumer/src/main/java/com/onefiter/UserConsumerApplication.java
@@ -0,0 +1,25 @@
+package com.onefiter;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient  //开启Eureka客户端发现功能
+public class UserConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserConsumerApplication.class, args);
+    }
+
+    /***
+     * 将RestTemplate的实例放到Spring容器中
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ user-consumer/src/main/java/com/onefiter/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.onefiter.controller;
+
+
+import com.onefiter.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consumer")
+public class UserController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    /****
+     * 在user-consumer服务中通过RestTemplate调用user-provider服务
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/{id}")
+    public User queryById(@PathVariable(value = "id")Integer id){
+//        String url = "http://localhost:18081/user/find/"+id;
+//        return restTemplate.getForObject(url,User.class);
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-provider");
+        ServiceInstance serviceInstance = instances.get(0);
+        String instanceUrl = "http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/user/find/"+id;
+        return restTemplate.getForObject(instanceUrl,User.class);
+    }
+}
